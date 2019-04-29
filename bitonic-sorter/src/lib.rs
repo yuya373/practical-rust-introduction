1fbeb74a83130035e1dd940e477e5f76814a4011
Yuya Minami <yuya373@me.com>
2019-04-30 01:29:30 +0900

    Add bitonic-sorter

---
+++ bitonic-sorter/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod first;
+pub mod fourth;
+pub mod second;
+pub mod third;
+pub mod utils;
+
+pub enum SortOrder {
+    Ascending,
+    Descending,
+}
---
+++ bitonic-sorter/src/utils.rs
@@ -0,0 +1,23 @@
+use rand::distributions::Standard;
+use rand::{Rng, SeedableRng};
+use rand_pcg::Pcg64Mcg;
+
+pub fn new_u32_vec(n: usize) -> Vec<u32> {
+    let mut rng = Pcg64Mcg::from_seed([0; 16]);
+    // let mut v = Vec::with_capacity(n);
+
+    // for _ in 0..n {
+    //     v.push(rng.sample(&Standard));
+    // }
+
+    // v
+    rng.sample_iter(&Standard).take(n).collect()
+}
+
+pub fn is_sorted_ascending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] <= pair[1])
+}
+
+pub fn is_sorted_descending<T: Ord>(x: &[T]) -> bool {
+    x.windows(2).all(|pair| pair[0] >= pair[1])
+}
